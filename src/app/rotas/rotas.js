181984603736c18294de60fd83217332c2216bed
Thyago Bezerra Lima <thyagobezerra8608@gmail.com>
2019-10-28 23:27:33 -0300

    adicionado marko

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -15,17 +15,19 @@ module.exports = (app) =>{
     });
 
     app.get('/livros', function(req, resp){
-        resp.send(
-            `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-            `
+        resp.marko(
+            require('../views/livros/lista/lista.marko'), {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Node avançado'
+                    }
+                ]
+            }
         );
     });
 }
