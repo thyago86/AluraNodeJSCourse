a5c309fa4201ec574bb7cca4eec53ecdf8ae3ca7
Thyago Bezerra Lima <thyagobezerra8608@gmail.com>
2019-10-29 23:41:40 -0300

    adicionado database

---
+++ src/app/infra/livro-dao.js
@@ -0,0 +1,16 @@
+class LivroDAO{
+    constructor(db){
+        this._db = db;
+    }
+
+    lista(callback){
+        this._db.all(
+            'SELECT * FROM livros',
+            (erro, resultados) =>
+                callback(erro, resultados)
+            
+        )
+    }
+}
+
+module.exports = LivroDAO;
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDAO = require('../../app/infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) =>{
     app.get('/', function(req, resp){
         resp.send(
@@ -15,20 +18,26 @@ module.exports = (app) =>{
     });
 
     app.get('/livros', function(req, resp){
-        resp.marko(
-            require('../views/livros/lista/lista.marko'), {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
-            }
-        );
-    });
-}
 
+        const livroDAO = new LivroDAO(db);
+
+        livroDAO.lista(function(erro, resultados){
+            resp.marko(
+                require('../views/livros/lista/lista.marko'), {
+                    livros: resultados
+                }
+            );
+
+        // db.all('SELECT * FROM livros', function(erro, resultados){
+        //     resp.marko(
+        //         require('../views/livros/lista/lista.marko'), {
+        //             livros: resultados
+        //         }
+        //     );
+        // });
+
+        
+        });
+    }
+    )
+}
